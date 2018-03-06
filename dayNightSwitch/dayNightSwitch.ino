5f6e90924c34fd76a9f4596df5cba0caa6215cb3
Jeppe Larsen <veirum@gmail.com>
2018-03-06 09:51:57 +0100

    Variables and Port Setup
    Added the needed varables and setting up the ports to OUTPUT

--- dayNightSwitch/dayNightSwitch.ino
+++ dayNightSwitch/dayNightSwitch.ino
@@ -1,9 +1,28 @@
+
+// Digital Ports
+int mRelayPort = 2; // Port for a mechanical relay
+int ssRelayPort = 3; // Port for a solid state relay
+
+// Analog ports
+int photoResistor = 0; // Photoresistor for sensing the amount of light
+int lightTresholdPot = 1; // Potmeter for adjusting the on/off threshold
+int onTimePot = 2; // Potmeter to adjust the ON time
+
+// Timer variables
+unsigned long currentMillis = 0; // used for storing the current milliseconds
+unsigned long oldMillis = 0; // used for storing milliseconds from the millis() function to compare time
+
+// Tresholds
+int onOffTreshold;
+
 void setup() {
-  // put your setup code here, to run once:
+  // Setting the relay ports to output
+  pinMode(mRelayPort, OUTPUT);
+  pinMode(ssRelayPort, OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
 
 }
